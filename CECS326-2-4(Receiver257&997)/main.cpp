e904e2072b2c4e6fa212c2c431c185df10bcfb87
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-13 15:34:53 -0700

    final update before testing

--- CECS326-2-4(Receiver257&997)/main.cpp
+++ CECS326-2-4(Receiver257&997)/main.cpp
@@ -5,17 +5,6 @@
  *      Author: lukecjm
  */
 
-/*
-This is a simple illustration of the use of:
-	ftok, msgget, msgsnd, msgrcv
-Program A will use a message queue created by Program B.
-Then, they will pass messages back and forth.
-Program A sends the first message and reads the reply. Program A
-also sends a "fake" message to the msgQ that will never be read
-by anyone.
-Both child processes use message type mtype = 113 and 114. (these two numbers will not be used)
-*/
-
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -30,19 +19,12 @@ using namespace std;
  * The second receiver gets messages from 257 & 997 senders.
  * The second receiver terminates after it has received a total of 5000
 messages. (Also, remember to send the ack to 997 sender.)
+Leaves no extra messages in the queue
  */
 int main()
 {
-	/*
-	0 -> assuming that the queue exists
-	ftok 	-> same identifier as the other program (these must match otherwise the queue will not be found)[passed parameters must be the same]
-		-> these programs must be in the same directory
-	*/
-	int qid = msgget(ftok(".",'u'), 0);
+	int qid = msgget(ftok("/volumes/USBDRIVE/eclipseworkspace/",'u'), 0);
 
-	/*
-	must be the same as other programs because the message objects in the queue are byte aligned
-	*/
 	// declare my message buffer
 	struct buf {
 		long mtype; // required
@@ -51,27 +33,64 @@ int main()
 	buf msg;
 	int size = sizeof(msg)-sizeof(long);
 
-	/*
-	getpid() -> gets an identifier for this program, not too useful here, but will be useful in later assignments
-	*/
-//	cout << getpid() << ": sends greeting" << endl;
+	//indicates if the sender is still sending
+	bool StillSending997 = true;
 	int messageCount = 0;
 
 	do
 	{
-	msgrcv(qid, (struct msgbuf *)&msg, size, 200, 0); // reading
-	cout << getpid() << ": gets message" << endl;
-	cout << "reply: " << msg.greeting << endl;
-	cout << getpid() << ": now exits" << endl;
+		if(StillSending997)
+		{
+			msgrcv(qid, (struct msgbuf *)&msg, size, 997, 0);
 
-	//figure out how to determine the sender
+			cout << "Receiver 200 gets message: " << msg.greeting << endl;
+
+			//will set the boolean to receive from the 997 sender if the message begins with 'L'
+			//	(expected "Last hello from sender 997")
+			if(msg.greeting[0] == 'L')
+			{
+				StillSending997 = false;
+			}
+
+			//putting this here instead of above will ensure that no extra ack
+			//	messages are sent back to sender 997 after it has terminated
+			if(StillSending997)
+			{
+				msg.mtype = 201;
+				strcpy(msg.greeting, "Goodbye from Receiver 200");
+				msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
+			}
+			messageCount++;
+		}
+
+		msgrcv(qid, (struct msgbuf *)&msg, size, 251, 0);
+
+		cout << "Receiver 200 gets message: " << msg.greeting << endl;
+		msg.mtype = 202;
+		strcpy(msg.greeting, "Goodbye from Receiver 200");
+		msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
+
+		messageCount++;
+	}while(messageCount < 5000);
+
+	//removes the excess received message from sender 997 if this receiver terminates before sender 997
+	if(StillSending997)
+	{
+		msgrcv(qid, (struct msgbuf *)&msg, size, 997, 0);
+	}
+
+	//termination notification sent to sender 997
 	msg.mtype = 201;
-	strcpy(msg.greeting, "Goodbye from Receiver 200");
-	msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocall
+	strcpy(msg.greeting, "Last goodbye from Receiver 200");
+	msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
 
-	messageCount++;
-	//find out how to tell when a sender stops sending
-	}while(messageCount <5000);
+	//termination notification sent to sender 257
+	msg.mtype = 202;
+	msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
+
+	//termination notification sent to sender 251
+	msg.mtype = 203;
+	msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
 
 	exit(0);
 }
