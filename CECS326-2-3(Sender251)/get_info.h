22a889707c7b4db9565a96b20e7eab8411fd463c
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-13 20:24:25 -0700

    update

--- CECS326-2-3(Sender251)/get_info.h
+++ CECS326-2-3(Sender251)/get_info.h
@@ -10,8 +10,20 @@
 
 #include<iostream>
 #include<signal.h>
-#include<unistd.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+
+// declare my message buffer
+	struct buf {
+		long mtype; // required long datatype, and required to be first
+		char greeting[50]; // mesg content
+	};
 
 void get_info(int qid, msgbuf *exitmsg, int size, long mtype);
 
