02f38b987f814e2805991666d7b3936a53f46940
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-13 20:17:50 -0700

    update

--- CECS326-2-5(Sender257)/main.cpp
+++ CECS326-2-5(Sender257)/main.cpp
@@ -13,6 +13,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <cstdlib>
+#include <cstdio>
+#include <limits.h>
 using namespace std;
 
 /*
@@ -22,8 +24,10 @@ receiver stops receiving event notification.
 
 int main() {
 	//qid -> an id that is internal to this program
-	int qid = msgget(ftok("/volumes/USBDRIVE/eclipseworkspace/",'u'), 0);
-
+	int qid = msgget(ftok("/Desktop/CECS326-Message-Queue-master/",'u'), 0);
+	
+	static char initialMessage[] = "Hello from Sender 257: ";
+	char* tempMessage;
 	// declare my message buffer
 	struct buf {
 		long mtype; // required long datatype, and required to be first
@@ -32,11 +36,12 @@ int main() {
 
 	buf msg;
 	int size = sizeof(msg)-sizeof(long);
+	char tempValue[9];
 
 	//seeds the rand() function
 	srand(time(NULL));
 	//gets a random unsigned int value [0 - 2^32] to be passed between programs
-	unsigned int randomUInt;
+	int randomUInt;
 
 	//indicates if this sender should continue sending messages to the receiver
 	bool StillReceiving200 = true;
@@ -46,13 +51,16 @@ int main() {
 		//get a random unsigned integer value that is divisible by 10
 		do
 		{
-			randomUInt  = UINT_MAX*rand();
+			randomUInt  = INT_MAX*rand();
 		}while(randomUInt%257 != 0);
+
+		cout << "257Active: " << randomUInt << endl;
 
 		//initializing message for receiver 200
 		msg.mtype = 257;
+		sprintf(tempValue, "%d", randomUInt);
 		strcpy(msg.greeting, "Hello from Sender 257: ");
-		strcat(msg.greeting, (char *)(randomUInt%257));
+		strcat(msg.greeting, tempValue);
 		msgsnd(qid, (struct msgbuf *)&msg, size, 0);
 
 		msgrcv(qid, (struct msgbuf *)&msg, size, 202, 0);
@@ -64,11 +72,12 @@ int main() {
 			StillReceiving200 = false;
 		}
 	} while(StillReceiving200);
+	cout << "257NotActive\n";
 
 	//empties the one extra message sent from this sender before exiting since its receiver has terminated
 	msgrcv(qid, (struct msgbuf *)&msg, size, 257, 0);
 
-	//send a message to the closing program (sender 251) that sender 257 has terminated
+	//send a message to the closing program (sender 997) that sender 257 has terminated
 	msg.mtype = 258;
 	strcpy(msg.greeting, "Last message sent by Sender 257");
 	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
