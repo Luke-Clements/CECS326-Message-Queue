b21f2df92cf91c7f7c86bbef1dffc520ffdb51d2
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-13 20:16:56 -0700

    update

--- CECS326-2-3(Sender251)/main.cpp
+++ CECS326-2-3(Sender251)/main.cpp
@@ -13,6 +13,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <cstdlib>
+#include <cstdio>
+#include "get_info.h"
+#include <limits.h>
 using namespace std;
 
 /*
@@ -21,7 +24,10 @@ to one receiver & terminates on a 'kill' command from a separate terminal.
  */
 int main() {
 	//qid -> an id that is internal to this program
-	int qid = msgget(ftok("/volumes/USBDRIVE/eclipseworkspace/",'u'), 0);
+	int qid = msgget(ftok("/Desktop/CECS326-Message-Queue-master/",'u'), 0);
+
+	static char initialMessage[] = "Hello from Sender 251: ";
+	char* tempMessage;
 
 	// declare my message buffer
 	struct buf {
@@ -29,11 +35,10 @@ int main() {
 		char greeting[50]; // mesg content
 	};
 
-	//holds info about the queue for when this program removes the queue
-	//	(used for the number of messages in the queue)
-	msqid_ds * queueInfo;
-
 	buf msg;
+	char tempValue[9];
+
+	strcpy(msg.greeting, "Last message sent from sender 251");
 
 	/*
 	Calculate the size of the message
@@ -41,10 +46,14 @@ int main() {
 	*/
 	int size = sizeof(msg)-sizeof(long);
 
+	get_info(qid, (struct msgbuf *)&msg, size, 251);
+
+	cout << getpid() << endl;
+
 	//seeds the rand() function
 	srand(time(NULL));
 	//gets a random unsigned int value [0 - 2^32] to be passed between programs
-	unsigned int randomUInt;
+	int randomUInt;
 
 	//initializing message for receiver 100
 	msg.mtype = 251;
@@ -54,45 +63,24 @@ int main() {
 		//get a random unsigned integer value that is divisible by 5
 		do
 		{
-			randomUInt  = UINT_MAX*rand();
+			randomUInt  = INT_MAX*rand();
 		}while(randomUInt%251 != 0);
+		cout << "251Active: " << randomUInt << " " << getpid() << endl;
 
-		strcpy(msg.greeting, "Hello from Sender 251");
-		strcat(msg.greeting, (char *)(randomUInt%251));
+		sprintf(tempValue, "%d", randomUInt);
+		strcpy(msg.greeting, "Hello from Sender 251: ");
+		strcat(msg.greeting, tempValue);
 		msgsnd(qid, (struct msgbuf *)&msg, size, 0);
 
+		cout << "gothere" << endl;
+
 	} while(true);
+	cout << "251NotActive\n";
 
-	//sends final message to receiver to the receiver can be terminated
-	msg.mtype = 251;
-	strcpy(msg.greeting, "Last message sent from sender 251");
-	msgsnd (qid, (struct msgbuf *)&msg, size, 0); //exit protocol
-
-	//waits for last message to be sent by 998 sender
-	msgrcv(qid, (struct msgbuf *)&msg, size, 998, 0);
-
-	//waits for last message to be sent by 257 sender
-	msgrcv(qid, (struct msgbuf *)&msg, size, 258, 0);
-
-	//waits for last message to be sent by 100 receiver
-	msgrcv(qid, (struct msgbuf *)&msg, size, 103, 0);
-
-	//waits for last message to be sent by 200 receiver
-	msgrcv(qid, (struct msgbuf *)&msg, size, 203, 0);
-
-	msgctl(qid, IPC_STAT, queueInfo);
-
-	cout << queueInfo->msg_qnum << endl;
-	//will empty the queue
-	while(queueInfo->msg_qnum > 0)
-	{
-		msgrcv(qid, (struct msgbuf *)&msg, size, 0, 0);
-	}
-
-	cout << queueInfo->msg_qnum << endl;
-
-	//removes the queue
-	msgctl(qid, IPC_RMID, NULL);
+	//send a message to the closing program (sender 997) that sender 251 has terminated
+	msg.mtype = 252;
+	strcpy(msg.greeting, "Last message sent by Sender 251");
+	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
 
 	exit(0);
 }
